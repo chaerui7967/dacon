b3e118054a398bc7fecda3f463f04dc1b778bc11
rui <chaerui7967@gmail.com>
2021-06-09 15:37:23 +0900

    coding start

--- electricity_amount/elec_EDA.py
+++ electricity_amount/elec_EDA.py
@@ -19,8 +19,12 @@ import os
 print(os.getcwd())  # 현재 디렉토리 확인
 warnings.filterwarnings('ignore')
 
-train_df = pd.read_csv('electricity_amount/energy/train.csv', encoding='cp949')
-
-
 # 데이터 불러오기
+train_df = pd.read_csv('electricity_amount/energy/train.csv', encoding='cp949')
+test_df = pd.read_csv('electricity_amount/energy/test.csv', encoding='cp949')
+sub = pd.read_csv('electricity_amount/energy/sample_submission.csv', encoding='cp949')
 
+# 데이터 정보확인
+train_df.info()
+train_df.shape
+train_df.columns
